1f928ecab7bde6658c13345e768755f1c33ebe40
LasithChamik <106394753+LasithChamik@users.noreply.github.com>
2022-06-07 22:47:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -34,16 +34,15 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('➕ Add me to a Your Group ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/MWUpdatez'),
-            InlineKeyboardButton('💠 UᎮDΛTΞS 💠', url='https://t.me/OpusTechz')
+            InlineKeyboardButton('🔰 GROUP 🔰', url='https://t.me/Cinema_World_Sir_Lanka_Group'),
+            InlineKeyboardButton('🔥 UPDATES 🔥, url='https://t.me/Cinema_World_Bot_Updates')
             ],[      
-            InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
-            InlineKeyboardButton('CONTACT ME', url='https://t.me/AboutAadhi'),
-            InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
+            InlineKeyboardButton('☘️ HELP ☘️', callback_data='help'),
+            InlineKeyboardButton('🔎 ABOUT 🔍', callback_data='about')
             ],[
-            InlineKeyboardButton('✅ SUBSCᏒIBΞ  ✅', url='https://youtube.com/channel/UCf_dVNrilcT0V2R--HbYpMA')
+            InlineKeyboardButton('🌺 JOIN OUR CHANNEL 🌺', url='https://t.me/Cinema_World_Sri_Lanka')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -78,16 +77,15 @@ async def start(client, message):
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('➕ Add me to a Your Group ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/MWUpdatez'),
-            InlineKeyboardButton('💠 UᎮDΛTΞS 💠', url='https://t.me/OpusTechz')
+            InlineKeyboardButton('🔰 GROUP 🔰', url='https://t.me/Cinema_World_Sir_Lanka_Group'),
+            InlineKeyboardButton('🔥 UPDATES 🔥, url='https://t.me/Cinema_World_Bot_Updates')
             ],[      
-            InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
-            InlineKeyboardButton('CONTACT ME', url='https://t.me/AboutAadhi'),
-            InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
+            InlineKeyboardButton('☘️ HELP ☘️', callback_data='help'),
+            InlineKeyboardButton('🔎 ABOUT 🔍', callback_data='about')
             ],[
-            InlineKeyboardButton('✅ SUBSCᏒIBΞ  ✅', url='https://youtube.com/channel/UCf_dVNrilcT0V2R--HbYpMA')
+            InlineKeyboardButton('🌺 JOIN OUR CHANNEL 🌺', url='https://t.me/Cinema_World_Sri_Lanka')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
